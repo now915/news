4715522188464608ff1aced972863997c8c68c2c
now915 <742766223@qq.com>
2019-08-06 18:03:31 +0800

    create

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,52 @@
+<!--  -->
+<template>
+    <svg :class="svgClass" aria-hidden="true">
+        <use :xlink:href="iconName" />
+    </svg>
+</template>
+
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    iconName () {
+      return `#icon-${this.iconClass}`
+    },
+    svgClass () {
+      if (this.className) {
+        return 'svg-icon-' + this.className
+      } else {
+        return 'svg-icon'
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.svg-icon,.svg-icon-small,.svg-icon-big{
+  vertical-align: middle;
+  fill: currentColor;
+}
+.svg-icon {
+  width: 10vw;
+  height: 10vw;
+}
+.svg-icon-small{
+  width: 5vw;
+  height: 5vw;
+}
+.svg-icon-big{
+  width: 20vw;
+  height: 20vw;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <svg-icon  icon-class="computer" />
+    <svg-icon  icon-class="computer" className="big" />
+    <svg-icon  icon-class="computer" className="small" />
+    <p style="background-color: orange">hellow world</p>
+    <div class="asd">
+      HelloWorld
+    </div>
   </div>
 </template>
 
@@ -16,3 +23,41 @@ export default {
   }
 }
 </script>
+<style scoped>
+/* .border_1px:before{
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 1px;
+  width: 100%;
+  background-color: #000;
+  transform-origin: 50% 0%;
+} */
+@media only screen and (-webkit-min-device-pixel-ratio:2){
+    .border_1px:before{
+        transform: scaleY(0.5);
+    }
+}
+@media only screen and (-webkit-min-device-pixel-ratio:3){
+    .border_1px:before{
+        transform: scaleY(0.33);
+    }
+}
+@svg border_1px {
+  height: 2px;
+  @rect {
+    fill: var(--color, black);
+    width: 100%;
+    height: 50%;
+  }
+}
+.asd{
+  height: 50vw;
+  text-align: left;
+  padding: 10vw;
+  position: relative;
+  border: 1px solid transparent;
+  border-image: svg(border_1px param(--color black)) 2 2 stretch;
+}
+</style>
